f2d5f7035a14cfae4666f784e45ad506d85b714a
SzKristof97 <47104780+SzKristof97@users.noreply.github.com>
2020-01-13 14:28:11 +0100

    Első feltöltés
    Már ugyan kész van a projekt, mivel egyszerű volt, de későbbi változtatások végett és könnyebb hozzáférhetőség miatt feltöltöm gitre!

---
+++ ListViewTestForm/ListViewTestForm/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ListViewTestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.PrimaryKey = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Text = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PrimaryKey,
+            this.Text});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(18, 18);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(232, 475);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // PrimaryKey
+            // 
+            this.PrimaryKey.Text = "Kulcs";
+            this.PrimaryKey.Width = 96;
+            // 
+            // Text
+            // 
+            this.Text.Text = "text";
+            this.Text.Width = 128;
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(336, 18);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(143, 26);
+            this.keyBox.TabIndex = 1;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(336, 50);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(143, 26);
+            this.textBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(257, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kulcs";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(257, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Text";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(261, 451);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(218, 42);
+            this.btn_add.TabIndex = 5;
+            this.btn_add.Text = "Hozzáadás";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.Btn_add_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 506);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader PrimaryKey;
+        private System.Windows.Forms.ColumnHeader Text;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_add;
+    }
+}
+
---
+++ ListViewTestForm/ListViewTestForm/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ListViewTestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MyInit();
+        }
+
+        private void MyInit()
+        {
+            //ListViewItem létrehozása
+            ListViewItem lvi = new ListViewItem();
+
+            lvi.Text = "Elsődleges Kulcs1";             //Ez az első oszlop a ListViewban
+            lvi.SubItems.Add("Szöveg1");      //... Minden további .add egy új oszlop!
+            listView1.Items.Add(lvi);       //Hozzá adjuk a ListViewItem -et a listához!
+
+            //Szemléltetés képpen létrehozok még 1 listViewItem-et
+            //Nem lehet 1-nél többször hozzá adni egy ListViewItemet, ezért minden sor egy új "lvi"
+            ListViewItem lvi2 = new ListViewItem();
+            lvi2.Text = "Elsődleges Kulcs2";
+            lvi2.SubItems.Add("Szöveg2");
+            listView1.Items.Add(lvi2);
+
+            //FONTOS, hogy ha manuálisan hozzuk létre a ListViewItemet
+            //akkor ugyan olyan nevű listát nem lehet 2x hozzá adni
+            //listView1.Items.add(lvi)      //<--- Ez a sor hibát dobna ki futási időben! Próbáld ki, vedd le a megjegyzést!
+        }
+
+        /*
+         * Példaképp létrehozok egy eljárást amivel új listákat lehet hozzá adni 1 parancsal!
+         */
+         private void AddNewList(List<string> items)
+        {
+            if (items.Count == 0) return; //Ha üres az items lista, akkor visszatér hogy elkerüljük a hibákat!
+
+            //Létrehozunk egy új ListViewItemet
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = items[0];        //Az első elemet hozzá adjuk manuálisan, ez az első oszlop!
+
+            for (int i = 1; i < items.Count; i++)
+            { //Minden további elem a listában egy új oszlop lesz
+                lvi.SubItems.Add(items[i]);
+            }
+
+            //Majd hozzá adjuk a ListView-hoz a ListViewItemet
+            listView1.Items.Add(lvi);
+        }
+
+        private void Btn_add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //létrehozzuk a listát
+                List<string> list = new List<string>();
+
+                //feltöltjük az elemeket
+                list.Add(keyBox.Text);
+                list.Add(textBox.Text);
+
+                //az általunk létrehozott eljárás alapján hozzá adunk egy új ListViewItemet!
+                AddNewList(list);
+            }
+            catch (Exception ex)
+            {
+                //hiba üzenet arra az esetre, ha valami nem sikerülne!
+                MessageBox.Show("Hiba lépett fel, Hibakód: \n" + ex.StackTrace,"Hiba!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
